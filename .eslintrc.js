5d017391b5d46b0e36b180ed02ebbecd44556f6b
Vyacheslav Oprysk <vyacheslav.oprysk@travisperkins.co.uk>
2020-09-06 12:52:21 +0300

    deploy to heroku

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: ["eslint:recommended", "prettier"],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
   rules: {
     "no-useless-escape": 0,
-    "no-console": 2
+    "no-console": 2,
   },
   overrides: [
     {
@@ -22,7 +22,7 @@ module.exports = {
       env: {
         browser: true,
         node: true,
-        es6: true
+        es6: true,
       },
       extends: [
         "eslint:recommended",
@@ -30,22 +30,22 @@ module.exports = {
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "prettier/@typescript-eslint"
+        "prettier/@typescript-eslint",
       ],
       parser: "@typescript-eslint/parser",
       parserOptions: {
         ecmaFeatures: {
-          jsx: true
+          jsx: true,
         },
         ecmaVersion: 2018,
         sourceType: "module",
-        project: "./tsconfig.json"
+        project: "./tsconfig.json",
       },
       plugins: ["react", "react-hooks", "@typescript-eslint"],
       settings: {
         react: {
-          version: "detect"
-        }
+          version: "detect",
+        },
       },
       rules: {
         "@typescript-eslint/no-inferrable-types": 0,
@@ -54,8 +54,8 @@ module.exports = {
           2,
           {
             arrayDestructuring: true,
-            objectDestructuring: true
-          }
+            objectDestructuring: true,
+          },
         ],
         "@typescript-eslint/array-type": [1, { default: "array-simple" }],
         "@typescript-eslint/camelcase": 0,
@@ -63,8 +63,8 @@ module.exports = {
         "no-useless-escape": 0,
         "no-console": 2,
         "react-hooks/rules-of-hooks": 2,
-        "react-hooks/exhaustive-deps": 1
-      }
-    }
-  ]
+        "react-hooks/exhaustive-deps": 1,
+      },
+    },
+  ],
 };
